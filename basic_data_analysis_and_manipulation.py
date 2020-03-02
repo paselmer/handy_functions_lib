591e759d1b5dc55a53c36d6a70a99b0a00f888c9
paselmer <37942073+paselmer@users.noreply.github.com>
2020-03-02 17:59:19 -0500

    Updated basic_data_analysis_and_manipulation.py - delineate_consecutive_binned_vector_values function [3/2/20]

--- basic_data_analysis_and_manipulation.py
+++ basic_data_analysis_and_manipulation.py
@@ -5,14 +5,20 @@ def delineate_consecutive_binned_vector_values(arr,bins):
         'arr' that fit within any of the given 'bins.'
         
         Return value change_indx will be an integer array containing the
-        indicies at with the values change to a different bin. 
+        indicies at which the values change to a different bin. 
         Return value ncounts will be the number of consecutive values in
         arr in a bin.
     """
     
-    # NOTES:
+    # INPUT:
+    # arr = a 1-D array (vector) containing the data values
+    # bins = list or array defining the bounds of the bins
+    
+    # OUTPUTS:
     # ui = the starting indicies of the consecutive value runs
     # ncounts = the count of each unique value run
+    # bin_numbers = the number of the bin you just entered into as corresponds
+    #     to the above output vectors (ui, ncounts).
     
     bin_numbers = np.digitize(arr,bins)
     dbn = bin_numbers[1:] - bin_numbers[:-1]
@@ -28,4 +34,11 @@ def delineate_consecutive_binned_vector_values(arr,bins):
     ui = np.zeros(change_indx.shape[0]+1,dtype=np.uint32)
     ui[1:] = change_indx
     
-    return ui, ncounts
+    # Reduce bin_numbers so that it corresponds to other output vectors
+    bn_out = np.zeros(ui.shape,dtype=bin_numbers.dtype)
+    bn_out[-1] = bin_numbers[-1]
+    bin_numbers = bin_numbers[:-1][change_bn_mask]
+    bn_out[:-1] = bin_numbers
+    bin_numbers = bn_out
+    
+    return ui, ncounts, bin_numbers
